d5e5b774489389a7c44698c2f5ed6007994cabd0
小松 <815693903@qq.com>
2023-05-23 18:08:42 +0800

    第一次提交src

---
+++ src/test/java/com/yxs/AppTest.java
@@ -0,0 +1,23 @@
+package com.yxs;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+//tgwrgwertjgp[eqjgptej[twe
+//jedis branch
+//gitee正在写了个注释 idea能看到吗
+public class AppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ src/test/java/com/yxs/JedisClient.java
@@ -0,0 +1,34 @@
+package com.yxs;
+
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import java.util.Map;
+
+
+public class JedisClient {
+//    单实例连接Redis
+    @Test
+    public void JedisClient(){
+//        String
+        Jedis jedis = new Jedis("192.168.40.129", 6379);
+        jedis.set("s8", "uzi");
+        String result = jedis.get("s8");
+        System.out.println(result);
+        jedis.close();
+    }
+
+    //使用jedis连接池连接Redis服务器
+    @Test
+    public void jedisPool(){
+        JedisPool jedisPool = new JedisPool("192.168.40.129", 6379);
+        Jedis jedis = jedisPool.getResource();
+        jedis.set("s9", "ig");
+        String s8 = jedis.get("s9");
+        System.out.println(s8);
+        jedis.close();
+        jedisPool.close();
+
+    }
+}
